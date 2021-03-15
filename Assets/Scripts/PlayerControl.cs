1d83fc2512aef71a42b036dcf1de39af962380ee
David Thong Nguyen <dev@davidtn.com>
2021-03-15 12:14:27 -0400

    Can walk but the idle animation state does not stop when moving

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,18 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+///require some things the bot control needs
+[RequireComponent(typeof(Animator), typeof(Rigidbody), typeof(CapsuleCollider))]
+//[RequireComponent(typeof(CharacterInputController))]
 public class PlayerControl : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody rbody;
-    private CharacterInputController cinput;
+    //private CharacterInputController cinput;
+
+    private Transform leftFoot;
+    private Transform rightFoot;
+
+
+    private float filteredForwardInput = 0f;
+    private float filteredTurnInput = 0f;
+
+    public bool InputMapToCircular = true;
+
+    public float forwardInputFilter = 5f;
+    public float turnInputFilter = 5f;
+
+    private float forwardSpeedLimit = 1f;
+
+
+    public float Forward
+    {
+        get;
+        private set;
+    }
+
+    public float Turn
+    {
+        get;
+        private set;
+    }
+
+    public bool Action
+    {
+        get;
+        private set;
+    }
+
+    public bool Jump
+    {
+        get;
+        private set;
+    }
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+
+        if (anim == null)
+            Debug.Log("Animator could not be found");
+
+        rbody = GetComponent<Rigidbody>();
+
+        if (rbody == null)
+            Debug.Log("Rigid body could not be found");
+
+        //cinput = GetComponent<CharacterInputController>();
+        //if (cinput == null)
+        //    Debug.Log("CharacterInput could not be found");
+    }
 
     public int moveSpeed = 5;
-    // Start is called before the first frame update
     void Start()
     {
-        
+        //example of how to get access to certain limbs
+        leftFoot = this.transform.Find("mixamorig:Hips/mixamorig:LeftUpLeg/mixamorig:LeftLeg/mixamorig:LeftFoot");
+        rightFoot = this.transform.Find("mixamorig:Hips/mixamorig:RightUpLeg/mixamorig:RightLeg/mixamorig:RightFoot");
+
+        if (leftFoot == null || rightFoot == null)
+            Debug.Log("One of the feet could not be found");
+
     }
+
+
 
     // Update is called once per frame
     void Update()
@@ -21,8 +88,23 @@ public class PlayerControl : MonoBehaviour
         float inputForward = 0f;
         float inputTurn = 0f;
 
+        //GetAxisRaw() so we can do filtering here instead of the InputManager
+        float h = Input.GetAxisRaw("Horizontal");// setup h variable as our horizontal input axis
+        float v = Input.GetAxisRaw("Vertical"); // setup v variables as our vertical input axis
+
+        if (InputMapToCircular)
+        {
+            // make coordinates circular
+            //based on http://mathproofs.blogspot.com/2005/07/mapping-square-to-circle.html
+            h = h * Mathf.Sqrt(1f - 0.5f * v * v);
+            v = v * Mathf.Sqrt(1f - 0.5f * h * h);
+
+        }
+
         if (Input.GetKey(KeyCode.W))
         {
+
+            //float inputForward = 0f;
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S))
@@ -38,7 +120,17 @@ public class PlayerControl : MonoBehaviour
             transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
         }
 
-        anim.SetFloat("velx", inputTurn);
-        anim.SetFloat("vely", inputForward);
+        //do some filtering of our input as well as clamp to a speed limit
+        filteredForwardInput = Mathf.Clamp(Mathf.Lerp(filteredForwardInput, v,
+            Time.deltaTime * forwardInputFilter), -forwardSpeedLimit, forwardSpeedLimit);
+
+        filteredTurnInput = Mathf.Lerp(filteredTurnInput, h,
+            Time.deltaTime * turnInputFilter);
+
+        Forward = filteredForwardInput;
+        Turn = filteredTurnInput;
+
+        anim.SetFloat("velx", Turn);
+        anim.SetFloat("vely", Forward);
     }
 }
