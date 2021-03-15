5df37c58b712dd513f9ed5e0d6f00261f6342e19
David Thong Nguyen <dev@davidtn.com>
2021-03-15 15:03:07 -0400

    Test Crouching

--- Assets/Scripts/RootMotionControlScript.cs
+++ Assets/Scripts/RootMotionControlScript.cs
@@ -163,6 +163,7 @@ public class RootMotionControlScript : MonoBehaviour
         anim.speed = this.animationSpeed;
         anim.SetFloat("velx", inputTurn);
         anim.SetFloat("vely", inputForward);
+        anim.SetBool("crouching", cinput.Crouch);
         anim.SetBool("isFalling", !isGrounded);
         anim.SetBool("doButtonPress", doButtonPress);
         anim.SetBool("matchToButtonPress", doMatchToButtonPress);
