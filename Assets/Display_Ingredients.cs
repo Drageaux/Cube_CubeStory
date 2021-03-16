7b2917282dea39b9257646e2cc0f845bacfca9b9
Ziwei-W426 <58496061+Ziwei-W426@users.noreply.github.com>
2021-03-15 21:38:26 -0400

    Ingredient_Display

---
+++ Assets/Display_Ingredients.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Display_Ingredients : MonoBehaviour
+{
+
+    public GameObject DisplayText;
+    private bool open = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayText.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.attachedRigidbody != null)
+        {
+                open = true;
+                //  trigger = true;
+                if (open)
+                {
+                    DisplayText.SetActive(true);
+                    StartCoroutine("WaitForSec");
+                }
+
+        }
+
+        //EventManager.TriggerEvent<BombBounceEvent, Vector3>(c.transform.position);
+        //Destroy(this.gameObject);
+
+
+
+    }
+
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(3);
+        DisplayText.SetActive(false);
+        // trigger = false;
+
+
+    }
+}
+
+
+
+    
+
+    
+    
+
