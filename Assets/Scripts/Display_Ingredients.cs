61b7853c94a3202084597ed908780aa3fd36bb9d
Ziwei-W426 <58496061+Ziwei-W426@users.noreply.github.com>
2021-03-15 23:16:16 -0400

    Ingredient_Collide
    The ingredient will disappear after the player close to it.

--- Assets/Scripts/Display_Ingredients.cs
+++ Assets/Scripts/Display_Ingredients.cs
@@ -5,41 +5,45 @@ using UnityEngine;
 public class Display_Ingredients : MonoBehaviour
 {
 
+    // Start is called before the first frame update
     public GameObject DisplayText;
     private bool open = false;
-
-    // Start is called before the first frame update
     void Start()
     {
         DisplayText.SetActive(false);
     }
+
     private void OnTriggerEnter(Collider c)
     {
         if (c.attachedRigidbody != null)
         {
-                open = true;
-                //  trigger = true;
-                if (open)
-                {
-                    DisplayText.SetActive(true);
-                    StartCoroutine("WaitForSec");
-                }
+            open = true;
+            //  trigger = true;
+            if (open)
+            {
+                DisplayText.SetActive(true);
+                StartCoroutine("WaitForSec");
+            }
 
         }
 
-        //EventManager.TriggerEvent<BombBounceEvent, Vector3>(c.transform.position);
-        //Destroy(this.gameObject);
-
+     
 
 
     }
-
     IEnumerator WaitForSec()
     {
         yield return new WaitForSeconds(3);
         DisplayText.SetActive(false);
         // trigger = false;
 
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
 
     }
 }
