690960e11247c387cf225df88974727417a5c342
Ziwei-W426 <58496061+Ziwei-W426@users.noreply.github.com>
2021-03-22 20:57:08 -0400

    Revise_Ingredient

--- Assets/Scripts/Ingredient_Collider.cs
+++ Assets/Scripts/Ingredient_Collider.cs
@@ -9,6 +9,7 @@ public class Ingredient_Collider : MonoBehaviour
     private bool touch = false;
     public GameObject ingredient;
 
+
     // Start is called before the first frame update
     void Start()
     {
