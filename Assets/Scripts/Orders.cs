697296c7385d2e44cc89a45c93d5bec4163c9977
David Thong Nguyen <dev@davidtn.com>
2021-03-25 18:19:43 -0400

    Calculate remaining time

--- Assets/Scripts/Orders.cs
+++ Assets/Scripts/Orders.cs
@@ -19,20 +19,54 @@ public class Orders : MonoBehaviour
 
     #endregion
 
+    public delegate void OnOrdersChanged();
+    public OnOrdersChanged onOrdersChangedCallback;
 
 
-    [System.Serializable]
-    public class Order
-    {
-        [HideInInspector]
-        public string name = "Order";
-        public Recipe dish;
-        public float remainingTime;
-    }
     public List<Order> orders = new List<Order>();
-
     public void Add(Order order)
     {
         orders.Add(order);
+        if (onOrdersChangedCallback != null)
+            onOrdersChangedCallback.Invoke();
+    }
+
+    public void Remove(Order order)
+    {
+        orders.Remove(order);
+    }
+
+    private void Update()
+    {
+        foreach (Order o in orders)
+        {
+            if (!o.completed)
+            {
+                o.decreaseTimer();
+            }
+        }
+    }
+}
+
+
+[System.Serializable]
+public class Order
+{
+    [HideInInspector]
+    public string name = "Order";
+    public Recipe dish;
+    public float timeToComplete = 60f;
+    public bool completed = false;
+
+    public float RemainingTime
+    {
+        get;
+        private set;
+    }
+
+    public void decreaseTimer()
+    {
+        RemainingTime = timeToComplete - Time.time;
+        Debug.Log("remaining time: " + RemainingTime);
     }
 }
