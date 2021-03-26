752787e00e4ff3602975307c4e1b891b3c2bc521
David Thong Nguyen <dev@davidtn.com>
2021-03-25 20:50:47 -0400

    Adding ordersUI file

---
+++ Assets/Scripts/OrdersUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrdersUI : MonoBehaviour
+{
+    Orders orders;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        orders = Orders.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateUI()
+    {
+        Debug.Log("UPDATING UI");
+    }
+}
