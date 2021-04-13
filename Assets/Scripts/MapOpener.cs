94489b1dd07df33d9380cffb34f4f439543af60a
SunnyMaddineni <smaddineni@yahoo.com>
2021-04-12 22:05:39 -0400

    basicMap

---
+++ Assets/Scripts/MapOpener.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapOpener : MonoBehaviour
+{
+    public GameObject panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+         if (Input.GetKeyDown(KeyCode.M)){
+             if (panel.activeSelf== false)
+            {
+                panel.SetActive(true);
+            }
+            else
+            {
+                panel.SetActive(false);
+            }
+         }
+    }
+    public void OpenPanel(){
+         if (panel.activeSelf== false)
+            {
+                panel.SetActive(true);
+            }
+            else
+            {
+                panel.SetActive(false);
+            }
+    }
+
+}
