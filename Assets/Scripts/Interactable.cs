f44af232001eea3f6e9a749a26f06c296d4275d9
David Thong Nguyen <dev@davidtn.com>
2021-04-17 00:23:37 -0400

    WIP: picking up items script and animation; also updating UI

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    // reference: https://www.youtube.com/watch?v=9tePzyL6dgc&list=PLPV2KyIb3jR4KLGCCAciWQ5qHudKtYeP7&index=3
+    public float radius = 3f;
+
+    bool isFocus = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        // This method is meant to be overwritten
+        Debug.Log("Interacting " + transform.name);
+    }
+
+    private void Update()
+    {
+        if (isFocus)
+        {
+            // TODO: change color
+        }
+    }
+
+    public void OnInteracted()
+    {
+        this.hasInteracted = true;
+    }
+
+    public void OnFocused()
+    {
+        isFocus = true;
+    }
+
+    public void OnDefocused()
+    {
+        isFocus = false;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
