846bc3b298d9cae15e8af3fc3c280d760da9add1
David Thong Nguyen <dev@davidtn.com>
2021-04-24 00:24:38 -0400

    Detect if player is dive catching

--- Assets/Scripts/AnimalIngredient.cs
+++ Assets/Scripts/AnimalIngredient.cs
@@ -11,6 +11,30 @@ public class AnimalIngredient: ItemPickup
         base.Start();
         name = ingredient.name;
         type = InteractableType.AnimalIngredient;
+        SphereCollider pickUpCollider = GetComponent<SphereCollider>();
+        pickUpCollider.radius = radius;
+    }
+
+    protected void OnCollisionEnter(Collider other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            try
+            {
+                if (other.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Dive Catch"))
+                {
+                    print("Caught");
+                } 
+                else
+                {
+                    print("Need to press E");
+                }
+            } 
+            catch
+            {
+                Debug.LogError("Cannot get Player's current animation state info");
+            }
+        }
     }
 
     private void OnDestroy()
