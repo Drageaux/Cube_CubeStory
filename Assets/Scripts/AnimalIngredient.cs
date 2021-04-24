dd05dc0da37fca17f5ea8f7d7e917c354243d911
David Thong Nguyen <dev@davidtn.com>
2021-04-23 23:19:14 -0400

    New Animal Ingredient script to allow character dive catching

---
+++ Assets/Scripts/AnimalIngredient.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalIngredient: ItemPickup
+{
+    public Ingredient ingredient;
+
+    protected override void Start()
+    {
+        base.Start();
+        name = ingredient.name;
+        type = InteractableType.AnimalIngredient;
+    }
+
+    private void OnDestroy()
+    {
+        this.OnDefocused();
+    }
+}
