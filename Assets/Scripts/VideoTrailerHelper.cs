d0fde5abf0a4f07fb9087e677ad1d0ca5b303c0b
David Thong Nguyen <dev@davidtn.com>
2021-04-28 12:46:37 -0400

    Hide UI if in trailer mode

--- Assets/Scripts/VideoTrailerHelper.cs
+++ Assets/Scripts/VideoTrailerHelper.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class VideoTrailerHelper : MonoBehaviour
 {
+    public List<CanvasGroup> uiCanvases = new List<CanvasGroup>();
+    public bool displayUI = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (displayUI)
+            gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (CanvasGroup c in uiCanvases)
+        {
+            if (c)
+                c.alpha = 0f;
+        }
     }
 }
