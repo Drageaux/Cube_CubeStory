ef0b1bd1c25621ffce318077278d675d199c585c
David Thong Nguyen <dev@davidtn.com>
2021-04-27 22:42:17 -0400

    New Trap Placer script

---
+++ Assets/Scripts/TrapPlacer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapPlacer : MonoBehaviour
+{
+    private Transform placeSpot;
+    public GameObject trapPrefab;
+
+    private CharacterInputController cinput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cinput = GetComponent<CharacterInputController>();
+        placeSpot = transform.Find("TrapPlaceSpot");
+    }
+
+    // Update is called once per frame
+    public void PlaceTrap()
+    {
+        GameObject trap = Instantiate(trapPrefab, placeSpot);
+        trap.transform.localPosition = Vector3.zero;
+        trap.transform.SetParent(null);
+    }
+}
