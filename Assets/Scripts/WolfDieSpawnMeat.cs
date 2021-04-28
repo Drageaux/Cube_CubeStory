891f0239d3d33bff0b4e2c1614071475b836c73b
David Thong Nguyen <dev@davidtn.com>
2021-04-28 12:33:00 -0400

    Better rotation for spawned wolf meat

--- Assets/Scripts/WolfDieSpawnMeat.cs
+++ Assets/Scripts/WolfDieSpawnMeat.cs
@@ -7,6 +7,6 @@ public class WolfDieSpawnMeat : MonoBehaviour
     public GameObject wolfMeatPrefab;
     public GameObject MeatSpawner()
     {
-        return Instantiate(wolfMeatPrefab, transform.position, transform.rotation);
+        return Instantiate(wolfMeatPrefab, transform.position, Quaternion.Euler(15f, transform.rotation.y, transform.rotation.z));
     }
 }
