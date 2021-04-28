83e6a5ef9e98fef59e5b49dee280e1df0ee58ca0
David Thong Nguyen <dev@davidtn.com>
2021-04-28 03:46:20 -0400

    Fixed popup blocking blocking raycasts

--- Assets/Scripts/popUp.cs
+++ Assets/Scripts/popUp.cs
@@ -6,20 +6,35 @@ public class popUp : MonoBehaviour
 {
     public GameObject popUpBox;
     public Animator animator;
+
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        canvasGroup = popUpBox.GetComponent<CanvasGroup>();
+        if (canvasGroup == null) {
+            Debug.LogError("component is null");
+        }
+        close();
+    }
+
     // Start is called before the first frame update
     public void PopUp()
     {
-        
-        popUpBox.SetActive(true);
-        animator.SetTrigger("pop");
         Time.timeScale = 0f;
-
-
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
+        Cursor.visible = true;
+        animator.SetTrigger("pop");
     }
     public void close()
     {
-       
-        animator.SetTrigger("close");
         Time.timeScale = 1f;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0f;
+        //Cursor.visible = false;
+        animator.SetTrigger("close");
     }
 }
