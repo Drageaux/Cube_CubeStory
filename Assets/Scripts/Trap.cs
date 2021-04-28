ef62d6e0222f05f4eb43be6cfdaf8ab072c2fb92
David Thong Nguyen <dev@davidtn.com>
2021-04-27 20:39:58 -0400

    Can spawn meat and destroy trap&wolf

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public GameObject trapped;
+    public Transform trappedSpot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trappedSpot = transform.Find("TrappedMeatSpot");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("wolf"))
+        {
+            print("killed wolf");
+            trapped = collider.gameObject.GetComponent<WolfDieSpawnMeat>().MeatSpawner();
+            trapped.SetActive(true);
+            trapped.transform.position = trappedSpot.position;
+            trapped.transform.SetParent(null);
+            Destroy(collider.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
